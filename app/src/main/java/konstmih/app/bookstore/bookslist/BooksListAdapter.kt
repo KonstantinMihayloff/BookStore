1cfcd49c4e7d94334d741a95092f2d7985e83416
Konstantin Mihayloff <mihayloff.konst@gmail.com>
2020-06-20 17:25:52 +0200

    FakeBook API created to simulated book sync

---
+++ app/src/main/java/konstmih/app/bookstore/bookslist/BooksListAdapter.kt
@@ -0,0 +1,61 @@
+package konstmih.app.bookstore.bookslist
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+import konstmih.app.bookstore.Book
+import konstmih.app.bookstore.R
+import kotlinx.android.synthetic.main.item_book.view.*
+
+class BooksListAdapter(private val books: List<Book>,
+                        private val listener: BooksListAdapterListener?)
+    : RecyclerView.Adapter<BooksListAdapter.ViewHolder>(), View.OnClickListener {
+
+    // Listener Interface
+    interface BooksListAdapterListener {
+
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        // Get from layouts
+        val cardView = itemView.findViewById<CardView>(R.id.cardView)!!
+        val bookCover = itemView.findViewById<ImageView>(R.id.bookCover)!!
+        val bookTitle = itemView.findViewById<TextView>(R.id.bookTitle)!!
+        val bookAuthor = itemView.findViewById<TextView>(R.id.bookAuthor)!!
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val viewItem = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_book, parent, false)
+        return ViewHolder(viewItem)
+    }
+
+    // Update while scroll
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val book = books[position]
+        with(holder) {
+            cardView.setOnClickListener(this@BooksListAdapter)
+            cardView.tag = book
+            bookTitle.text = book.title
+            bookAuthor.text = book.author
+
+            // Get image from pictureUrl
+            Picasso.get()
+                .load(book.pictureUrl)
+                .placeholder(R.drawable.ic_placeholder_image)
+                .into(bookCover)
+        }
+    }
+
+    // Get Item Count
+    override fun getItemCount() = books.size
+
+    override fun onClick(v: View?) {
+        TODO("Not yet implemented")
+    }
+}
