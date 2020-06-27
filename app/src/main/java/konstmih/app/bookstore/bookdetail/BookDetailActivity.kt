0596e9af3b0f5944617fe3c44a00686ca38f6b2d
Konstantin Mihayloff <mihayloff.konst@gmail.com>
2020-06-27 14:41:02 +0200

    Displaying book details

--- app/src/main/java/konstmih/app/bookstore/bookdetail/BookDetailActivity.kt
+++ app/src/main/java/konstmih/app/bookstore/bookdetail/BookDetailActivity.kt
@@ -3,7 +3,10 @@ package konstmih.app.bookstore.bookdetail
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.squareup.picasso.Picasso
+import konstmih.app.bookstore.Book
 import konstmih.app.bookstore.R
 
 import kotlinx.android.synthetic.main.activity_book_detail.*
@@ -22,7 +25,6 @@ class BookDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_book_detail)
-        setSupportActionBar(toolbar)
 
         // Get intent
         val bookId = intent.getIntExtra(EXTRA_BOOK_ID, 1)
@@ -32,7 +34,20 @@ class BookDetailActivity : AppCompatActivity() {
         // A factory is used here so that after a screen rotation, the same view model will be used
         val factory = BookDetailViewModelFactory(bookId)
         viewModel = ViewModelProviders.of(this, factory).get(BookDetailViewModel::class.java)
+        viewModel.book.observe(this, Observer { book -> updateBook(book!!) })
+    }
 
+    private fun updateBook(book: Book) {
+        // Cover
+        Picasso.get()
+            .load(book.pictureUrl)
+            .placeholder(R.drawable.ic_placeholder_image)
+            .into(bookCover)
+
+        // Info
+        bookTitle.text = book.title
+        bookAuthor.text = book.author
+        bookSummary.text = book.summary
     }
 
 }
--- app/src/main/java/konstmih/app/bookstore/bookdetail/BookDetailViewModelFactory.kt
+++ app/src/main/java/konstmih/app/bookstore/bookdetail/BookDetailViewModelFactory.kt
@@ -3,6 +3,8 @@ package konstmih.app.bookstore.bookdetail
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
+// A factory is used to be able to use the same ViewModel after a screen rotation
+// It allows also to pass bookId as a parameter of the ViewModel
 class BookDetailViewModelFactory(private val bookId: Int)
     : ViewModelProvider.NewInstanceFactory(){ // This class will be a ViewModel Factory
 
